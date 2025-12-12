c116ffcc4357cad13f7714a07863fb9a824a838e
Joshua Selbo <jselbo@meta.com>
2025-12-12 15:01:59 -0600

    Add tooling for Spotless + ktfmt

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,6 @@
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
---
+++ build-logic/src/main/kotlin/mockito-kotlin.quality-spotless-conventions.gradle.kts
@@ -0,0 +1,11 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
+plugins {
+    id("com.diffplug.spotless")
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        ktfmt().kotlinlangStyle()
+    }
+}
